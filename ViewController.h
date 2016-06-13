5435c9ea2316a7781d219e4eb077ff61dc6e013e
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-13 16:19:47 -0400

    Initial Commit

---
+++ ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  SQLite3DBSample
+//
+//  Created by Gabriel Theodoropoulos on 25/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EditInfoViewController.h"
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, EditInfoViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tblPeople;
+
+
+- (IBAction)addNewRecord:(id)sender;
+
+@end
